01fee5db268839a3858d5cf4db32d9d6995227c4
jordan <jordanjcastro8@gmail.com>
2021-05-11 10:12:12 -0400

    Styling

--- src/app/dishes.service.ts
+++ src/app/dishes.service.ts
@@ -9,17 +9,17 @@ export class DishesService {
   dishes: Dish[] = [
     {
      name: "Fish Tacos", cuisine: "Mexican",
-     price: 12.99, description: "Write description here...",
+     price: 12.99, description: "A taco is a traditional Mexican dish consisting of a small hand-sized corn or wheat tortilla topped with a filling. The tortilla is then folded around the filling and eaten by hand.",
      image: "https://toriavey.com/images/2012/07/Crispy-Fish-Tacos-.jpg"
     },
     {
      name: "Bulalo", cuisine: "Filipino",
-     price: 14.99, description: "Write description here...",
+     price: 14.99, description: "Bulalô is a beef dish from the Philippines. It is a light colored soup that is made by cooking beef shanks and bone marrow until the collagen and fat has melted into the clear broth.",
      image: "https://panlasangpinoy.com/wp-content/uploads/2018/07/How-to-Cook-Bulalo-Recipe.jpg"
     },
     {
      name: "Yangchow", cuisine: "Chinese",
-     price: 10.99, description: "Write description here...",
+     price: 10.99, description: "Yangzhou fried rice, Yangchow fried rice, or Yeung Chow fried rice is a popular Chinese-style wok fried rice dish in many Chinese restaurants throughout the world.",
      image: "https://philippinesfoodrecipes.files.wordpress.com/2016/02/yang-chow-fried-rice.jpg"
     }
   ];
